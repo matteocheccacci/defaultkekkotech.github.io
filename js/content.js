1ee860a352b71dede6cb8852ebf37a7d73835bdd
matteocheccacci <92668748+matteocheccacci@users.noreply.github.com>
2025-04-24 01:56:55 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -19,7 +19,7 @@ Lo stile minimal e il font Consolas riflettono la mia passione per la programmaz
 - Progetto 1: <a href='https://downloads.kekkotech.com/projects/progetto1/progetto1.html' target='_blank'>Vai a Progetto1</a>
 - Progetto 2: <a href='https://downloads.kekkotech.com/projects/progetto2/progetto2.html' target='_blank'>Vai a Progetto2</a>`,
 
-  contact: `Contatti:
+  contacts: `Contatti:
 Email: <a href='mailto:matteo@example.com'>matteo@example.com</a>
 Instagram: <a href='https://instagram.com/matteo.checcacci' target='_blank'>@matteo.checcacci</a>`
 };
