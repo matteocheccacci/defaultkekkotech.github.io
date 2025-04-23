d1ae60d60880de9ce54f6057e55dd00d5c6b1880
matteocheccacci <92668748+matteocheccacci@users.noreply.github.com>
2025-04-23 19:01:33 +0200

    Delete welcome.php

--- welcome.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["nome"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-Your email address is: <?php echo $_POST["messaggio"]; ?>
-
-</body>
-</html>
