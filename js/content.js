482429302e4f8d18acfeaf3219797e7777f96dac
matteocheccacci <92668748+matteocheccacci@users.noreply.github.com>
2025-04-23 20:32:17 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,21 +1,25 @@
-
- const siteContent = {
-   help: `Elenco dei comandi:
-   home: per tornare all'home page
-   about: per scoprire qualcosa in più su chi sono e cosa faccio
-   downloads: per scaricare i progetti che ho realizzato
-   didattica (non attivo): per accedere alla pagina relativa alla didattica
-   contacts: per un elenco dei contatti`,
-   homemobile: `Benvenuto su kekkotech.com!
- Tocca un comando per esplorare.`,
+const siteContent = {
+  help: `Elenco dei comandi:
+home: per tornare all'home page
+about: per scoprire qualcosa in più su chi sono e cosa faccio
+downloads: per scaricare i progetti che ho realizzato
+didattica (non attivo): per accedere alla pagina relativa alla didattica
+contacts: per un elenco dei contatti`,
+  
   home: `Benvenuto su kekkotech.com!
-   Digita 'help' per iniziare ad eslporare`,
-   about: `Questo sito è progettato per sembrare un terminale, ma con pulsanti cliccabili per una navigazione immediata su dispositivi mobili.
- Lo stile minimal e il font Consolas riflettono la mia passione per la programmazione.`,
-   downloads: `Progetti scaricabili:
- - Progetto 1: <a href='https://kekkotech.com/downloads/progetto1.zip' target='_blank'>Scarica Progetto 1</a> (2.3 MB)
- - Progetto 2: <a href='https://kekkotech.com/downloads/progetto2.zip' target='_blank'>Scarica Progetto 2</a> (5.7 MB)`,
-   contact: `Contatti:
- Email: <a href='mailto:matteo@example.com'>matteo@example.com</a>
- Instagram: <a href='https://instagram.com/matteo.checcacci' target='_blank'>@matteo.checcacci</a>`
- };
+Digita 'help' per iniziare ad esplorare`,
+
+  homemobile: `Benvenuto su kekkotech.com!
+Tocca un comando per esplorare.`,
+
+  about: `Questo sito è progettato per sembrare un terminale, ma con pulsanti cliccabili per una navigazione immediata su dispositivi mobili.
+Lo stile minimal e il font Consolas riflettono la mia passione per la programmazione.`,
+
+  downloads: `Progetti scaricabili:
+- Progetto 1: <a href='https://kekkotech.com/downloads/progetto1.zip' target='_blank'>Scarica Progetto 1</a> (2.3 MB)
+- Progetto 2: <a href='https://kekkotech.com/downloads/progetto2.zip' target='_blank'>Scarica Progetto 2</a> (5.7 MB)`,
+
+  contact: `Contatti:
+Email: <a href='mailto:matteo@example.com'>matteo@example.com</a>
+Instagram: <a href='https://instagram.com/matteo.checcacci' target='_blank'>@matteo.checcacci</a>`
+};
